f8c175e7eef89206a9519a835ac5f87384fe7336
kirawu <605018887@qq.com>
2018-09-14 16:50:51 +0800

    添加jsonarray解析

--- kiralibrary/src/main/java/com/aplus/kira/kiralibrary/tools/MGson.java
+++ kiralibrary/src/main/java/com/aplus/kira/kiralibrary/tools/MGson.java
@@ -2,6 +2,12 @@ package com.aplus.kira.kiralibrary.tools;
 
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by kirawu on 2017/9/29.
@@ -13,4 +19,17 @@ public class MGson {
         return parseResult;
     }
 
+    public static <T> List<T> fromJsonArray(String result, Class<T> clazz)
+    {
+        Type type = new TypeToken<ArrayList<JsonObject>>()
+        {}.getType();
+        ArrayList<JsonObject> jsonObjects = new Gson().fromJson(result, type);
+
+        List<T> arrayList = new ArrayList<>();
+        for (JsonObject jsonObject : jsonObjects)
+        {
+            arrayList.add(new Gson().fromJson(jsonObject, clazz));
+        }
+        return arrayList;
+    }
 }
